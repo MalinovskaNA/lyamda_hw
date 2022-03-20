35be9d82e4ba9d76da52e44ac78ad94650cfbe91
MalinovskaNA <delenmir@yandex.ru>
2022-03-20 20:54:42 +0300

    home work lyamds

---
+++ src/ru/netology/lessonN/Calculator.java
@@ -0,0 +1,31 @@
+package ru.netology.lessonN;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) ->  {
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            System.out.println("No zero division");
+        }
+        return 0;
+    };
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+
+    // Подсказка: обратите внимание на реализацию тернарного оператора в переменной abs.
+    // Для решения проблемы можете реализовать аналог, либо сделать полноценный многострочный блок кода с { return }
+}
---
+++ src/ru/netology/lessonN/Main.java
@@ -0,0 +1,17 @@
+package ru.netology.lessonN;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply( a,  b);
+
+        calc.println.accept(c);
+        // деление на 0, надо devide сделать не Integer
+
+    }
+}
